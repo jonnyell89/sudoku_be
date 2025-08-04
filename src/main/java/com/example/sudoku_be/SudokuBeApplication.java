7276385c3e1e7db3fe54972c291930e83f3f800c
jonnyell89 <jonnyell89@gmail.com>
2025-08-04 13:07:06 +0100

    initial commit - Spring Boot setup

---
+++ src/main/java/com/example/sudoku_be/SudokuBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sudoku_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sudoku_be/SudokuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sudoku_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
